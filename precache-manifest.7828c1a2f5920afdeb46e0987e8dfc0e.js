fd0205a713613f3ee9d8eb80e9a60e6dcdbd85d2
TaAaAa7 <mostafa.ahmed.abdelfattah@gmail.com>
2020-04-19 04:19:10 +0000

    Updates

---
+++ precache-manifest.7828c1a2f5920afdeb46e0987e8dfc0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a157a3a5cc77e0ff0aa6a4c4f4d915e3",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "28c7eed1d2b862a0fdfd",
+    "url": "/react_test/static/css/main.b537c495.chunk.css"
+  },
+  {
+    "revision": "6d186b87670455fb2a56",
+    "url": "/react_test/static/js/2.2edcbea6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_test/static/js/2.2edcbea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c7eed1d2b862a0fdfd",
+    "url": "/react_test/static/js/main.45280a51.chunk.js"
+  },
+  {
+    "revision": "c0af8a18745b9e9eaf4d",
+    "url": "/react_test/static/js/runtime-main.8a4449d4.js"
+  }
+]);
